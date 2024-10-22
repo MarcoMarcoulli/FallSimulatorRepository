dc829eaf1334893ba5b51af1753aedb8522f4f3c
MarcoMarcoulli <loca.marco02@gmail.com>
2024-10-23 00:15:25 +0200

    Automatic code generation from class diagram with Papyrus Designer

---
+++ Code/org.eclipse.papyrus.javagen.FallSimulator/src/main/java/ingdelsw/org/eclipse/papyrus/javagen/FallSimulator/App.java
@@ -0,0 +1,13 @@
+package ingdelsw.org.eclipse.papyrus.javagen.FallSimulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
