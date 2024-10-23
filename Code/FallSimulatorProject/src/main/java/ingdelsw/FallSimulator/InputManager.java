d4cc6bafb6d52b367454abb3b5caeca61daf9ffd
MarcoMarcoulli <loca.marco02@gmail.com>
2024-10-23 10:59:52 +0200

    Refactoring InputManager

--- Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulator/InputManager.java
+++ Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulator/InputManager.java
@@ -6,45 +6,37 @@ package ingdelsw.FallSimulator;
 
 import ingdelsw.FallSimulator.Math.Point;
 
-/************************************************************/
-/**
- * 
- */
 public class InputManager {
-	/**
-	 * 
-	 */
-	private Point startPoint;
-	/**
-	 * 
-	 */
-	private Point endPoint;
-
-	/**
-	 * 
-	 * @return 
-	 */
+	
+	private Point startPoint, endPoint;
+	
 	public Point getStartPoint() {
+		return startPoint;
 	}
 
-	/**
-	 * 
-	 * @return 
-	 */
 	public Point getEndPoint() {
+		return endPoint;
 	}
 
-	/**
-	 * 
-	 * @param startPoint 
-	 */
+	
 	public void setStartPoint(Point startPoint) {
+		this.startPoint = startPoint;
 	}
 
-	/**
-	 * 
-	 * @param endPoint 
-	 */
+	//controllo che il punto di arrivo sia più basso del punto di partenza
 	public void setEndPoint(Point endPoint) {
+		if(endPoint.getY()<=startPoint.getY()) 
+    		throw new IllegalArgumentException("Il punto di arrivo deve essere più in basso di quello di partenza");
+    	else if(endPoint.getX() == startPoint.getX()) //faccio in modo che i punti di partenza e arrivo non siano esattamente sulla stessa verticale per evitare curve degeneri
+    		this.endPoint = new Point(endPoint.getX()+1, endPoint.getY());
+    	else {
+    		this.endPoint=endPoint;
+    	}
 	}
+	
+	// Metodo per cancellare i punti inseriti
+    public void clearInput() {
+        startPoint=null;
+        endPoint=null;
+    }
 }
