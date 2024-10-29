af79bc7e6ed543d0dfe23f5a1e7ae518279c5ac4
MarcoMarcoulli <loca.marco02@gmail.com>
2024-10-29 08:20:44 +0100

    Curve class refactoring and DrawCurve method implementation

--- Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulator/Math/Curves/Curve.java
+++ Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulator/Math/Curves/Curve.java
@@ -7,19 +7,29 @@ package ingdelsw.FallSimulator.Math.Curves;
 import java.util.ArrayList;
 
 import ingdelsw.FallSimulator.Math.Point;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public abstract class Curve {
 	
 	protected double intervalX,intervalY;
+	protected Point startPoint, endPoint;
 
 	public Curve(Point startPoint, Point endPoint)
 	{
+		this.startPoint = startPoint;
+		this.endPoint = endPoint;
 		intervalX = endPoint.getX()-startPoint.getX();
 		intervalY = endPoint.getY()-startPoint.getY();
 	}
 	
-	public abstract ArrayList<Point> calculatePointList(Point startPoint, int numPoints);
+	public abstract ArrayList<Point> calculatePointList();
 	
-	public void drawCurve(Point startPoint, int numPoints) {
-	}
+	public void drawCurve(GraphicsContext gc) {
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(2);
+        ArrayList<Point> points = calculatePointList();
+        for (int i = 0; i < points.size() - 1; i++) 
+        	gc.strokeLine(points.get(i).getX(), points.get(i).getY(), points.get(i+1).getX(), points.get(i+1).getY());
+    }
 }
