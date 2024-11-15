d8b26251bc6c231ef8be57c8475143ea3a6e4e7c
MarcoMarcoulli <loca.marco02@gmail.com>
2024-11-16 00:14:48 +0100

    creating Mass constructor

--- Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulator/Mass.java
+++ Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulator/Mass.java
@@ -4,19 +4,24 @@
 
 package ingdelsw.FallSimulator;
 
+import ingdelsw.ExecutablePrototype.MassIcon;
 import ingdelsw.FallSimulator.Math.Point;
+import javafx.scene.image.ImageView;
 
-/************************************************************/
-/**
- * 
- */
 public class Mass {
-	/**
-	 * 
-	 */
-	private MassIcon iconType;
-	/**
-	 * 
-	 */
+	
 	private Point position;
+	private MassIcon iconType;
+	private final double massDiameter = 40;
+	
+	public Mass(Point startPosition, MassIcon iconType, ImageView icon) {
+        this.position = startPosition;
+        this.icon = icon;
+        this.icon.setX(Position.getX() - massDiameter/2);
+        this.icon.setY(Position.getY() - massDiameter/2);
+        this.iconType = iconType;
+        icon.setFitWidth(massDiameter);
+        icon.setFitHeight(massDiameter);
+    }
+	
 }
