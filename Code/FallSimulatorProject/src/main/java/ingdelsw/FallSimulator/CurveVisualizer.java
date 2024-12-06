2376323f996809201896897e104a0359a64147db
MarcoMarcoulli <loca.marco02@gmail.com>
2024-12-06 13:00:35 +0100

    implementing curves visualization

---
+++ Code/FallSimulatorProject/src/main/java/ingdelsw/FallSimulator/CurveVisualizer.java
@@ -0,0 +1,18 @@
+package ingdelsw.FallSimulator;
+
+import java.util.Random;
+
+import ingdelsw.FallSimulator.Math.Point;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class CurveVisualizer {
+		
+		public static void drawCurve(Point[] points, GraphicsContext gc, int red, int green , int blue) {
+			
+	        gc.setStroke(Color.rgb(red, green, blue));
+	        gc.setLineWidth(2);
+	        for (int i = 0; i < points.length - 1; i++) 
+	        	gc.strokeLine(points[i].getX(), points[i].getY(), points[i+1].getX(), points[i+1].getY());
+		}
+}
